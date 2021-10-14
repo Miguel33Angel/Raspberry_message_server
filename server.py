380ef8d7f2130eada0c6985e7420f34dd4adf6f8
Mangel Calvera <m.angel.calvera@gmail.com>
2021-10-14 02:23:04 +0200

    Basic functionality completed: Forms sends a post request that change internal values.

--- server.py
+++ server.py
@@ -1,22 +1,45 @@
 # Basic server used in a raspberry pi for connecting to a ESP32 that acts as a beeper
-from flask import Flask, render_template
+# Important: the max num of characters that can be sent is 15 (defined in the HTML)
+# Todo: security checks in the form.
+# Todo: better looking form
+# Todo: login page to introduce new ESP32
+
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+users = [
+        {'id': 0, 'name': 'Raquel', 'last_m': 'Hi!'},
+        {'id': 1, 'name': 'Mangel', 'last_m': 'Hi!'}
+    ]
 
 
 @app.route('/')
 @app.route('/home')
 def home_page():
-    users = [
-        {'id': 1, 'name': 'Raquel'},
-        {'id': 2, 'name': 'Mangel'}
-    ]
     return render_template('home.html', users=users)
 
-@app.route('/message')
+
+@app.route('/', methods=['POST'])
+def receive_message():
+    for user in users:
+        key = "message_to_"+user['name']
+        message = request.form[key]
+        if len(message) > 0:
+            user['last_m'] = message
+
+    return render_template('home.html', users=users)
 
 
-# if __name__ == '__main__':
-#     print("Server running")
+@app.route('/message/<ESP32_id>', methods=['GET'])
+def send_message(ESP32_id):
+    n = int(ESP32_id)
+    return "Hola "+users[n].get('name')+"! ESP n:"+str(n)
+
+
+if __name__ == '__main__':
+    # app.run(debug=True, port=8800) Only if no terminal is being used
+    print("Server running")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
